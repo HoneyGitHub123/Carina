cc231b5bb70d7fea7695abb48ce7f6e172e9d54d
HoneyGitHub123 <honeytom01@gmail.com>
2023-07-21 14:22:24 -0400

    Carin Login Web Test using dataprovider(updated)

--- src/test/java/com/company/carina/demo/WebLoginTest.java
+++ src/test/java/com/company/carina/demo/WebLoginTest.java
@@ -2,8 +2,9 @@ package com.company.carina.demo;
 
 
 import com.company.carina.demo.gui.components.LoginWindow;
+import com.company.carina.demo.gui.components.header.HeaderMenuBase;
 import com.company.carina.demo.gui.pages.common.LoginPageBase;
-import com.company.carina.demo.gui.pages.desktop.LoginPage;
+import com.company.carina.demo.gui.pages.desktop.HomePage1;
 import com.zebrunner.carina.core.IAbstractTest;
 import com.zebrunner.carina.core.registrar.ownership.MethodOwner;
 import org.testng.Assert;
@@ -15,16 +16,19 @@ public class WebLoginTest implements IAbstractTest {
     @Test(dataProvider = "TestData1")
     @MethodOwner(owner = "webdemo")
     public void loginTest(String email, String password, String expectedMessage) {
-        LoginPageBase loginPage = new LoginPage(getDriver());
-        loginPage.open();
-        loginPage.clickLoginLink();
-        LoginWindow loginWindow=loginPage.getLoginWindow();
-        //checking whether login window is displayed
-        Assert.assertTrue(loginWindow.isUIObjectPresent(),"Login Window is not displayed");
+        HomePage1 homePage = new HomePage1(getDriver());
+        homePage.open();
+        Assert.assertTrue(homePage.isPageOpened(), "Home page is not opened");
+        HeaderMenuBase headerMenu = homePage.getHeaderMenu();
+        LoginWindow loginWindow = headerMenu.clickLoginlink();
+        Assert.assertTrue(loginWindow.isUIObjectPresent(), "Login Window is not displayed");
+        loginWindow.checkEmail();
         loginWindow.enterEmail(email);
+        loginWindow.checkPassword();
         loginWindow.enterPassword(password);
-        //loginWindow.clickLoginButton();
-        String actualMessage = loginWindow.clickLoginButton().getMessage();
+        LoginPageBase loginPage = loginWindow.clickLoginButton();
+        Assert.assertTrue(loginPage.isPageOpened(), "Login page is not opened");
+        String actualMessage = loginPage.getMessage();
         System.out.println(actualMessage);
         SoftAssert softAssert = new SoftAssert();
         softAssert.assertEquals(actualMessage, expectedMessage, "Expected Message is not displayed");
