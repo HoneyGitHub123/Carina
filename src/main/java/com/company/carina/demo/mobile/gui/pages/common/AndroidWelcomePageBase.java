c7a4796d86ff5857bfd4b1d6e7ba523f2bc9db30
HoneyGitHub123 <honeytom01@gmail.com>
2023-07-31 07:23:34 -0400

    Carina Android Login Test(updated as per reviews)

--- src/main/java/com/company/carina/demo/mobile/gui/pages/common/AndroidWelcomePageBase.java
+++ src/main/java/com/company/carina/demo/mobile/gui/pages/common/AndroidWelcomePageBase.java
@@ -4,6 +4,7 @@ import com.zebrunner.carina.webdriver.gui.AbstractPage;
 import org.openqa.selenium.WebDriver;
 
 public abstract class AndroidWelcomePageBase extends AbstractPage {
+
     public AndroidWelcomePageBase(WebDriver driver) {
         super(driver);
     }
